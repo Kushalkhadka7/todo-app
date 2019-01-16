345c2b6e8d7d922260e28f010baaef2cbbba7de9
kushal <crkushal7@gmail.com>
2019-01-16 10:02:15 +0545

    added gh-pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import './assets/css/index.css';
-import AppRouter from './components/AppRouter';
+import App from './components/todo/App';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
